3393523aebf5e90b8e4e391f35ef195a92f5a3a2
Robin Tran <duytran.it@gmail.com>
2014-07-25 09:33:01 +0700

    change storage to store uploaded images

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,9 @@ class Book < ActiveRecord::Base
 	belongs_to :category
 	belongs_to :user
 
-	has_attached_file :photo, :styles => { :small => "150x150>" }
+	has_attached_file :photo, :styles => { :small => "150x150>" },
+								:url => "/assets/products/:id/:style/:basename.:extension",
+								:path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
 
 	validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
